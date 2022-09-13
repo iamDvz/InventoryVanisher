f9f28eba75786d12bfa2d6487ac5e124e08c452f
Denis <73883591+iamDvz@users.noreply.github.com>
2022-09-13 15:55:15 +0300

    clean up + refactor

--- src/main/java/ru/iamdvz/inventoryvanisher/InventoryVanisher.java
+++ src/main/java/ru/iamdvz/inventoryvanisher/InventoryVanisher.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.iamdvz.inventoryvanisher.commands.InventoryCommands;
 import ru.iamdvz.inventoryvanisher.listeners.InventoryListener;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -11,21 +12,23 @@ public final class InventoryVanisher extends JavaPlugin {
     private static InventoryVanisher instance;
     private static List<String> inventoryNames;
     private static boolean vanishOnAllNames;
-    private static List<String> defaultMenuNames;
     private static List<String> blacklistMenuNames;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         instance = this;
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new InventoryListener(), this);
         Objects.requireNonNull(this.getCommand("ivanisher")).setExecutor(new InventoryCommands());
     }
+    @Override
+    public void onLoad() {
+        instance = this;
+        loadFromConfig();
+    }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
     public static InventoryVanisher getInstance() {
@@ -37,16 +40,20 @@ public final class InventoryVanisher extends JavaPlugin {
     public static boolean getVanishOnAllNames() {
         return vanishOnAllNames;
     }
-    public static List<String> getDefaultMenuNames() {
-        return defaultMenuNames;
-    }
     public static List<String> getBlacklistMenuNames() {
         return blacklistMenuNames;
     }
     public static void loadFromConfig() {
         inventoryNames = instance.getConfig().getStringList("menu-names");
         vanishOnAllNames = instance.getConfig().getBoolean("vanish-on-all-menu-names");
-        defaultMenuNames = instance.getConfig().getStringList("default-menu-names");
         blacklistMenuNames = instance.getConfig().getStringList("blacklist-menu-names");
+
+        if (inventoryNames == null) {
+            inventoryNames = Collections.singletonList(" ");
+        }
+
+        if (blacklistMenuNames == null) {
+            blacklistMenuNames = Collections.singletonList(" ");
+        }
     }
 }
--- src/main/java/ru/iamdvz/inventoryvanisher/listeners/InventoryListener.java
+++ src/main/java/ru/iamdvz/inventoryvanisher/listeners/InventoryListener.java
@@ -6,7 +6,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import ru.iamdvz.inventoryvanisher.InventoryVanisher;
 
 import java.util.HashMap;
@@ -19,23 +18,21 @@ public class InventoryListener implements Listener {
     public void onInventoryOpen(InventoryOpenEvent event) {
         String eventTitle = event.getView().getTitle();
         if (((!event.getInventory().getType().defaultTitle().equals("Crafting"))
-                && (!InventoryVanisher.getDefaultMenuNames().contains(eventTitle))
                 && InventoryVanisher.getVanishOnAllNames())
                 && (!InventoryVanisher.getBlacklistMenuNames().contains(eventTitle))
                 || (InventoryVanisher.getInventoryNames().contains(eventTitle)
-                    && (!InventoryVanisher.getBlacklistMenuNames().contains(eventTitle)))) {
+                && (!InventoryVanisher.getBlacklistMenuNames().contains(eventTitle)))) {
             HumanEntity ePlayer = event.getPlayer();
-            PlayerInventory ePlayerInv = ePlayer.getInventory();
-            playerInventories.put(ePlayer, ePlayerInv.getContents());
-            ePlayerInv.clear();
+            playerInventories.put(ePlayer, ePlayer.getInventory().getContents());
+            ePlayer.getInventory().clear();
         }
     }
+
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         HumanEntity ePlayer = event.getPlayer();
         String eventTitle = event.getView().getTitle();
         if (((!event.getInventory().getType().defaultTitle().equals("Crafting"))
-                && (!InventoryVanisher.getDefaultMenuNames().contains(eventTitle))
                 && (InventoryVanisher.getVanishOnAllNames())
                 && (!InventoryVanisher.getBlacklistMenuNames().contains(eventTitle))
                 || (InventoryVanisher.getInventoryNames().contains(eventTitle)
@@ -43,8 +40,8 @@ public class InventoryListener implements Listener {
                 && playerInventories.containsKey(ePlayer)
                 && playerInventories.get(ePlayer) != null))) {
             ePlayer.getInventory().setContents(playerInventories.get(ePlayer));
+
         }
 
     }
-
 }
